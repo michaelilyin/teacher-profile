4ca6455552075a3f570cbddd5e4dab44bf1b3a8a
Anjytka <ann.nazarova@gmail.com>
2017-11-12 14:10:42 +0300

    Contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -6,6 +6,9 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit, OnDestroy {
+  balloonHeader = 'МБОУ СОШ №37';
+  balloonBody = '<img class="page_avatar_img" src="/assets/images/mbou_sosh_37.jpg" alt="МБОУ СОШ №37" width="200" height="200">';
+  balloonFooter = '';
 
   constructor() { }
 
